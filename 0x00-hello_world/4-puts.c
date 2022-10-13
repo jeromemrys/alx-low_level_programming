6733fb320578538896eef70990c7bb78a4d170d4
jeromemrys <jeromemrys@gmail.com>
2022-10-13 14:16:06 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main -Entry
- * returm: always 0
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
