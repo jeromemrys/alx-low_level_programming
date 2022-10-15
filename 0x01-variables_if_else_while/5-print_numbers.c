6f5e83abf220c57d3e8a271d0a5b6bf016f25698
jeromemrys <jeromemrys@gmail.com>
2022-10-14 18:55:20 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints numbers frok 0 to 9
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (int x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
