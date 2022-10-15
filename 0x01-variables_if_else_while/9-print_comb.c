01df45cb09c0fb366710e28acb60e6dd9561f715
jeromemrys <jeromemrys@gmail.com>
2022-10-15 02:58:38 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print all combinations of single digit
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+		if (x != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
