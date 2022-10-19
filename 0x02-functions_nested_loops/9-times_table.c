9c68052afd8d312483ad2f39be5ff43f58f93ef9
jeromemrys <jeromemrys@gmail.com>
2022-10-19 04:07:00 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,33 +9,19 @@ void times_table(void)
 
 	for (x = 0; x <= 9; x++)
 	{
-		for (y = 0; y <= 9; y++)
+		_putchar(0);
+
+		for (y = 1; y <= 9; y++)
 		{
+			_putchar(' ');
+			_putchar(' ');
 			z = x * y;
-			if (z < 10 && y > 0)
-			{
+			if (z <= 9)
 				_putchar(' ');
-			}
-			if (z >= 10)
-			{
-				_putchar(z / 10 + 48);
-				_putchar(z % 10 + 48);
-			}
-			if (y < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
 			else
-			{
-				_putchar(z + 48);
-			}
-			if (y < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			_putchar('\n');
+				_putchar((z / 10) + '0');
+			_putchar((z % 10) + '0');
 		}
+		_putchar('\n');
 	}
 }
