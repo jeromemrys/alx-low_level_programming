71f0e71aa64f6403f732590b0eb85a3f438db664
jeromemrys <jeromemrys@gmail.com>
2022-10-19 03:41:44 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * times_table - prints 9 times tabke starting from 0
+ * Return: 0
+ */
+void times_table(void)
+{
+	int x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+			if (z < 10 && y > 0)
+			{
+				_putchar(' ');
+			}
+			if (z >= 10)
+			{
+				_putchar(z / 10 + 48);
+				_putchar(z % 10 + 48);
+			}
+			if (y < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(z + 48);
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
