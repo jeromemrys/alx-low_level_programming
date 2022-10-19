66d854d50b154b3b6563dd201ab135f3ae9a0be6
jeromemrys <jeromemrys@gmail.com>
2022-10-18 19:39:40 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets in lowercase 10x and a new line
+ * Returm: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char lc;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (lc = 'a'; lc <= 'z'; lc++)
+		{
+			_putchar(lc);
+		}
+	}
+
+	_putchar('\n');
+}
