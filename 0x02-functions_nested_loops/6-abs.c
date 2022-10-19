e981caa419b9d3ecaa91fd51c13f0587b3a05c02
jeromemrys <jeromemrys@gmail.com>
2022-10-19 00:34:23 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - print absolute value of an integer
+ * @x: parameter to modify
+ * Return: absolute value
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	}
+	else
+	{
+		return (-x);
+	}
+}
