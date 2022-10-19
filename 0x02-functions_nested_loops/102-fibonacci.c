5ecd2015978d0aa5c3499e6fb68795b5d6122c4b
jeromemrys <jeromemrys@gmail.com>
2022-10-19 10:35:45 -0700

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints firat 50 fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	long int w, x, y, z, t;
+
+	w = 1;
+	x = 2;
+	y = 0;
+
+	for (t = 0; t < 33; t++)
+	{
+		if (w % 2 == 0 && w <= 4000000)
+		{
+			y = y + w;
+		} else if (w > 4000000)
+		{
+			break;
+		}
+		z = w;
+		w = x;
+		x = z + w;
+	}
+	printf("%ld\n", y);
+	return (0);
+}
