651eb890681417a904792111e6ecf403c090afd6
jeromemrys <jeromemrys@gmail.com>
2022-10-18 19:21:16 -0700

    Tsk 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,14 @@
  * print_alphabet - prints alphabets in lowercase
  * Return: 0
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char lc;
 
 	for (lc = 'a'; lc <= 'z'; lc++)
+	{
 		_putchar(lc);
+	}
 
 	_putchar('\n');
 	
