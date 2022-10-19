2115311693efafb9cc33ff83e34863138e80381f
jeromemrys <jeromemrys@gmail.com>
2022-10-18 20:17:04 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - check uppercase and lowercase
+ * @c: the character to check
+ * Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (C >= 'A' && C <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
