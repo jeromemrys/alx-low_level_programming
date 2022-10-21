a47afa8cff804b43838c5c6e2214be872a4ac06f
jeromemrys <jeromemrys@gmail.com>
2022-10-20 19:40:13 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,14 @@
 /**
  * _isupper - checks for uppercase
  * @c: character to check for uppercase
- * Return: 1 or 0
+ * Return: 0 or 1
  */
 
 int _isupper(int c)
+
 {
 	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
+		return  (1);
 	else
-	{
 		return (0);
-	}
 }
