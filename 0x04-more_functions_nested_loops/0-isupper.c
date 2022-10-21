dc1a173bac62eae8466ff2a925a1d8286b4ced95
jeromemrys <jeromemrys@gmail.com>
2022-10-20 19:46:36 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
  * _isupper - checks for uppercase
- * @c: character to check for uppercase
- * Return: 0 or 1
+ * @c: character to check
+ *
+ * Return: Always 0
  */
 
 int _isupper(int c)
