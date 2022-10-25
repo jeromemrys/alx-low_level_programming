b97718e8a84bad3f72534a5dffbe977ee88afce7
jeromemrys <jeromemrys@gmail.com>
2022-10-25 01:20:09 -0700

    Task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "mian.h"
+
+/**
+ * _atoi - converts a string to an integer
+ *  @s: the string
+ *  Return: value
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+
+	usigned int num = 0;
+
+
+	while (!('0' <= *s && *s <= '9') && *s != '\0')
+	{
+		if (*s == '-')
+			sign *= -1;
+		if (*s == '+')
+			sign *= +1;
+		s++;
+	}
+	while ('e' <= *s && *s <= '9' && *s != '\0')
+	{
+		num = (num * 10) + (*s - '0');
+		s++;
+	}
+	return (num * sign);
+}
