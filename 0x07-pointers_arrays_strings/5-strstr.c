43297cd70b2df2ee9f3d8cc0d4084559d7499d00
jeromemrys <jeromemrys@gmail.com>
2022-11-01 17:01:34 -0700

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,28 +4,28 @@
  * _strstr - locates a substring
  * @haystack: the longer string to search
  * @needle: the first occurrence of the substring
- * Return: a pointer beg of substring or @NULL if it not found
+ * Return: char value
  */
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int i, j = 0;
+	int i = 0, j = 0;
 
 	while (haystack[i])
 	{
-		while (needle[j] && (haystack[i] == needle[0]))
+		while (needle[j])
 		{
-			if (haystack[i + j] == needle[j])
-				j++;
-			else
+			if (haystack[i + j] != needle[j])
+			{
 				break;
+			}
+			j++;
 		}
-		if (needle[j])
+		if (needle[j] == '\0')
 		{
-			i++;
-			j = 0;
+			return (haystack + i);
 		}
-		else
-			return (haystack + 1);
+
+		i++;
 	}
-	return (0);
+	return ('\0');
 }
