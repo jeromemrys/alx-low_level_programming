63e0bc47a6e67ae7d8b18657de9e2d44e2916f6a
jeromemrys <jeromemrys@gmail.com>
2022-11-11 11:25:41 -0800

    added files

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates a memory
+ * @b: size
+ * Return: the memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *str;
+
+	str = malloc(b);
+	if (s == NULL)
+		exit(98);
+
+	return (s);
+}
